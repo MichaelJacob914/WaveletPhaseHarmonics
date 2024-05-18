d544c233ba933da6f2207a8937d7fdbbc71adfc1
MichaelJacob914 <102377228+MichaelJacob914@users.noreply.github.com>
2024-05-18 18:34:04 -0400

    Update fisher_S2Wav.py
    Renamed variables + Added timer to measure speed of calculations

--- fisher_S2Wav.py
+++ fisher_S2Wav.py
@@ -16,6 +16,7 @@ import torch
 import WPH_S2Wav
 from WPH_S2Wav import WaveletPhaseHarmonics
 from mpi4py import MPI
+import timeit
 
 d = np.load('maps_fisher_64.npy', allow_pickle = True)
 
@@ -43,6 +44,7 @@ F = d_again[header]
 while run_count < number_of_runs:
     comm = MPI.COMM_WORLD
     if run_count+comm.rank<number_of_runs:
+        start = timeit.timeit()
         sim_init = np.zeros(64**2*12)
         counter = 0
         for pixel in F[run_count][0]:
@@ -52,24 +54,26 @@ while run_count < number_of_runs:
         #sim_init = hp.ud_grade(sim_init,nside_out=64)
         #sim = torch.from_numpy(sim_init).to(device).contiguous()
         wph = WaveletPhaseHarmonics(tensor_field = sim_init, J = 5, L = 2, J_min = 3, azimuthal = azimuthal, polar = polar, nside=nside, device = 'cpu')
-        #wph.s00_calculator()
-        #wph.s01_calculator()
-        #wph.s11_calculator()
-        #wph.c01_calculator()
+        wph.s00_calculator()
+        wph.s01_calculator()
+        wph.s11_calculator()
+        wph.c01_calculator()
         wph.c00_calculator()
-        #s00, s00_indices = wph.get_coeffs("S00")
-        #s01, s01_indices = wph.get_coeffs("S01")
-        #s11, s11_indices = wph.get_coeffs("S11")
-        #c01, c01_indices = wph.get_coeffs("C01")
+        s00, s00_indices = wph.get_coeffs("S00")
+        s01, s01_indices = wph.get_coeffs("S01")
+        s11, s11_indices = wph.get_coeffs("S11")
+        c01, c01_indices = wph.get_coeffs("C01")
         c00, c00_indices = wph.get_coeffs("C00")
-        list_temp = list()
-        #list_temp.append(s00)
-        #list_temp.append(s01)
-        #list_temp.append(s11)
-        #list_temp.append(c01)
-        list_temp.append(c00)
-        list_full.append(list_temp)
+        coeffs = list()
+        coeffs.append(s00)
+        coeffs.append(s01)
+        coeffs.append(s11)
+        coeffs.append(c01)
+        coeffs.append(c00)
+        list_full.append(coeffs)
+        end = timeit.timeit()
         print("doing interation", run_count)
+        print('time taken', end - start)
     run_count+=comm.size
     comm.bcast(run_count,root = 0)
     comm.Barrier() 
